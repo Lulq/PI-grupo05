26c45751d9496a048c9c8dff9d99beff91274eb3
Luiz Negrinho <luizavm@gmail.com>
2021-04-02 12:03:37 -0300

    carregamento dos bancos de dados

--- index.js
+++ index.js
@@ -3,22 +3,40 @@ var moment = require("moment");
 let fs = require('fs');
 const { min } = require("moment"); // ver se precisa mesmo
 
-let db = fs.readFileSync('./db-livros.json'); // lê um arquivo json que contem informações sobre os livros cadastrados
 
-db = JSON.parse(db);
+let dbLivros = fs.readFileSync('./db.json'); // Lê um json.
+dbLivros = JSON.parse(dbLivros); // Converse o json em objeto JS
 
+// Carrega o banco de dados de usuários
+let dbUsers = fs.readFileSync('./db-users.json');
+dbUsers = JSON.parse(dbUsers);
+
+// Caminhos - facilita a ação da função atualizarBanco, tornando ela multifuncional (pode atualizar ambos os bancos de dado de acordo com o parâmetro passado)
+livros = ['db-livros.json', dbLivros];
+usuarios = ['db-users.json', dbUsers];
+
+//função para retornar a data e hora
 const dataAgora = () => {
     let data = moment().format("L - LTS")
     return data
 };
 
-const atualizarBanco = () => {
+//função que atualiza ambos os bancos de dados
+const atualizarBanco = (banco) => {
     //conversão do objeto JS para JSON
-    let livrosAtualizados = JSON.stringify(db, null, 2);
+    let atualizacao = JSON.stringify(banco[1], null, 2);
     // atualização do arquivo JSON do banco de dados
-    fs.writeFileSync('db-pets.json', livrosAtualizados, 'utf-8') // os parâmetros são o arquivo onde vai ser salvo, o que vai ser salvo nele, e a codificação do texto
+    fs.writeFileSync(banco[0], atualizacao, 'utf-8') // os parâmetros são o arquivo onde vai ser salvo, o que vai ser salvo nele, e a codificação do texto
 };
+
+const adicionarUsuario (novoUsuario) => {
+    db.users.push(novoUsuario)
+    atualizarBanco();
+};
+
+const adicionarLivro {};
 
 const listarLivros = () => {
     //lista todos os livros cadastrados
+
 }
